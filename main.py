f9868b25edaa470717e82a92a26ea207a7393920
danilshatalin <130211552+danilshatalin@users.noreply.github.com>
2023-04-18 19:56:10 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
-# Задача 1
-n = int(input())
-count_zero = 0
-count_one = 0
-for i in range(n):
-    x = int(input())
-    if x == 0:
-        count_zero += 1
-    else:
-        count_one += 1
-if count_one > count_zero:
-    print(count_zero)
-else:
-    print(count_one)
+# Задача 16
+import random
+N = int(input())
+X = int(input())
+A = [random.randint(0,10) for x in range(N)]
+count = 0
+for i in A:
+    if i == X:
+        count += 1
+print(count)
+
+
     
-# Задача 2
-x = int(input())
-y = int(input())
-for i in range(x):
-    for j in range(y):
-        if x == i + j and y == i * j:
-            print(i, j)
+# Задача 18
+import random
+N = int(input())
+X = int(input())
+A = [random.randint(0,N) for x in range(N)]
+t = X
+r = 0
+for i in A:
+    if ((X - i)**2)**0.5 < t:
+        r = i
+        t = ((X - i)**2)**0.5
+print(A)
+print(r)
+
  
-# Задача 3
-n = int(input())
-i = 0
-while 2 ** i <= n
-    print(2 ** i)
-    i += 1
+# Задача 20
+D = {1:'AEIOULNSTR', 2: 'DG', 3:'BCMP', 4:'FHVWY', 5:'K', 8:'JX', 10:'QZ'}
+r = 0
+word = input().upper()
+
+for i in word:
+    for k in D.keys():
+        if i in D[k]:
+            r += k
+print(r)
