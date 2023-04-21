f566a61dade762b9a45c83060827b5c99defad97
danilshatalin <130211552+danilshatalin@users.noreply.github.com>
2023-04-22 01:02:04 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,19 @@
-# Задача 16
-import random
-N = int(input())
-X = int(input())
-A = [random.randint(0,10) for x in range(N)]
-count = 0
-for i in A:
-    if i == X:
-        count += 1
-print(count)
+# Задача 26
+
+def sqr(a,b):
+    if b > 1:
+        return a * sqr(a,b - 1)
+    else:
+        return a
+
+print(sqr(2,8))
 
 
-    
-# Задача 18
-import random
-N = int(input())
-X = int(input())
-A = [random.randint(0,N) for x in range(N)]
-t = X
-r = 0
-for i in A:
-    if ((X - i)**2)**0.5 < t:
-        r = i
-        t = ((X - i)**2)**0.5
-print(A)
-print(r)
+# Задача 28
 
- 
-# Задача 20
-D = {1:'AEIOULNSTR', 2: 'DG', 3:'BCMP', 4:'FHVWY', 5:'K', 8:'JX', 10:'QZ'}
-r = 0
-word = input().upper()
-
-for i in word:
-    for k in D.keys():
-        if i in D[k]:
-            r += k
-print(r)
+def sum(a,b):
+    if b > 0:
+        return sum(a + 1,b - 1)
+    else:
+        return a
+print(sum(0,0))
