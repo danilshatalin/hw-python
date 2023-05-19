b735bcd4fa2aaf1c6837d4b273b9a8cc2209612b
danilshatalin <130211552+danilshatalin@users.noreply.github.com>
2023-05-19 16:07:08 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
-# Задача 26
+# Задача 34
 
-def sqr(a,b):
-    if b > 1:
-        return a * sqr(a,b - 1)
-    else:
-        return a
+def counter(phrase:str) -> int:
+    voewls = 'аиеёоуыэюя'
+    num = 0
+    for l in phrase:
+        if l in voewls:
+            num = num + 1
+    return num
 
-print(sqr(2,8))
+def parse_poem(poem: str) -> str:
+    num_phrase = poem.lower().split()
+    num_l = counter(poem[0])
+    for p in num_phrase[1:]:
+        if num_l != counter(p):
+            return "Пам парам"
+    return "Парам пам-пам"
 
 
-# Задача 28
+# Задача 36
 
-def sum(a,b):
-    if b > 0:
-        return sum(a + 1,b - 1)
-    else:
-        return a
-print(sum(0,0))
+def print_operation_table(operation,num_rows,num_colums):
+    for i in range(1,num_rows):
+        k = ""
+        for j in range(1,num_colums):
+            k = k + operation(i,j) + " "
+        print(k)
+ print_operation_table((lambda x,y: str(x*y)),7,7)
